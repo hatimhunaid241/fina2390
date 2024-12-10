e5a8e3c61ece7a114360604158d6e881254e0376
hatimhunaid241 <hatim@connect.hku.hk>
2024-12-10 22:36:55 +0800

    Completed Week 2

---
+++ 1_bisection_search.py
@@ -0,0 +1,26 @@
+import timeit
+
+start = timeit.default_timer()
+
+x = 1.5
+epsilon = 0.001
+numGuesses = 0
+low = 0.0
+high = max(1.0, x)
+ans = (high + low) / 2.0
+
+while abs(ans**2 - x) >= epsilon:
+    print("low =", low, "high =", high, "ans =", ans)
+    numGuesses += 1
+    if ans**2 < x:
+        low = ans
+    else:
+        high = ans
+    ans = (high + low) / 2.0
+print("numGuesses =", numGuesses)
+print(ans, "is close to square root of", x)
+
+print(
+    f"Total configuration execution time: {(timeit.default_timer() - start):.4f}s.",
+    flush=True,
+)
